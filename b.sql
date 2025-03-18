7aba54b71d635adf7b9b15403822a6eae7b5f5a0
Simon Gray <36992829+sfgray26@users.noreply.github.com>
2025-03-18 11:26:07 -0400

    Update b.sql

--- b.sql
+++ b.sql
@@ -3,21 +3,25 @@ BEGIN
     SET @sql = @sql + '
     SELECT 
         t.[Date],
-        COUNT(' + QUOTENAME(@currentCommentColumn) + ') AS Volume,
-        COUNT(DISTINCT CASE WHEN ' + QUOTENAME(@currentCommentColumn) + ' IS NOT NULL THEN t.[Date] END) AS DistinctVolume,
+        COUNT(CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX))) AS Volume,
+        COUNT(DISTINCT CASE WHEN CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX)) IS NOT NULL 
+                            AND DATALENGTH(CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX))) > 0 THEN t.[Date] END) AS DistinctVolume,  -- Changed to DATALENGTH
+        COUNT(*) AS TotalRows,
         ''' + @currentTableName + ''' AS TableName,
-        (SELECT TOP 1 ' + QUOTENAME(@currentCommentColumn) + '  -- Removed 'r.' and simplified subquery
+        (SELECT TOP 1 CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX))
          FROM ' + @currentTable + ' sub
-         WHERE ' + QUOTENAME(@currentCommentColumn) + ' IS NOT NULL
+         WHERE CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX)) IS NOT NULL
+         AND DATALENGTH(CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX))) > 0  -- Changed to DATALENGTH
          AND CAST(sub.[Load_Dt] AS DATE) = t.[Date]
          AND CAST(sub.[Load_Dt] AS DATE) BETWEEN ''' + CONVERT(VARCHAR(10), @startDate, 120) + ''' AND ''' + CONVERT(VARCHAR(10), @endDate, 120) + '''
          ORDER BY (SELECT NULL)) AS SampleComment
     FROM (
         SELECT 
             CAST([Load_Dt] AS DATE) AS [Date],
-            ' + QUOTENAME(@currentCommentColumn) + '
+            CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX))
         FROM ' + @currentTable + '
-        WHERE ' + QUOTENAME(@currentCommentColumn) + ' IS NOT NULL
+        WHERE CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX)) IS NOT NULL
+        AND DATALENGTH(CAST(' + QUOTENAME(@currentCommentColumn) + ' AS NVARCHAR(MAX))) > 0  -- Changed to DATALENGTH
         AND CAST([Load_Dt] AS DATE) BETWEEN ''' + CONVERT(VARCHAR(10), @startDate, 120) + ''' AND ''' + CONVERT(VARCHAR(10), @endDate, 120) + '''
     ) t
     GROUP BY t.[Date]';
