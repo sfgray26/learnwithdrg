76199b5c39bb1aab07bd976aae9c2101203cb05c
Simon Gray <36992829+sfgray26@users.noreply.github.com>
2025-03-18 10:00:05 -0400

    Update b.sql

--- b.sql
+++ b.sql
@@ -6,26 +6,19 @@ BEGIN
         COUNT(' + QUOTENAME(@currentCommentColumn) + ') AS Volume,
         COUNT(DISTINCT CASE WHEN ' + QUOTENAME(@currentCommentColumn) + ' IS NOT NULL THEN t.[Date] END) AS DistinctVolume,
         ''' + @currentTableName + ''' AS TableName,
-        (SELECT TOP 1 r.' + QUOTENAME(@currentCommentColumn) + '  -- Added 'r.' to reference the alias and corrected correlation
-         FROM ' + @currentTable + ' r
-         WHERE r.' + QUOTENAME(@currentCommentColumn) + ' IS NOT NULL
-         AND CAST(r.[Load_Dt] AS DATE) = t.[Date]
-         AND CAST(r.[Load_Dt] AS DATE) BETWEEN ''' + CONVERT(VARCHAR(10), @startDate, 120) + ''' AND ''' + CONVERT(VARCHAR(10), @endDate, 120) + '''  -- Added date range
+        (SELECT TOP 1 ' + QUOTENAME(@currentCommentColumn) + '  -- Removed 'r.' and simplified subquery
+         FROM ' + @currentTable + ' sub
+         WHERE ' + QUOTENAME(@currentCommentColumn) + ' IS NOT NULL
+         AND CAST(sub.[Load_Dt] AS DATE) = t.[Date]
+         AND CAST(sub.[Load_Dt] AS DATE) BETWEEN ''' + CONVERT(VARCHAR(10), @startDate, 120) + ''' AND ''' + CONVERT(VARCHAR(10), @endDate, 120) + '''
          ORDER BY (SELECT NULL)) AS SampleComment
     FROM (
         SELECT 
             CAST([Load_Dt] AS DATE) AS [Date],
-            ' + QUOTENAME(@currentCommentColumn) + ' AS ' + QUOTENAME(@currentCommentColumn) + '  -- Added AS clause for clarity
+            ' + QUOTENAME(@currentCommentColumn) + '
         FROM ' + @currentTable + '
         WHERE ' + QUOTENAME(@currentCommentColumn) + ' IS NOT NULL
         AND CAST([Load_Dt] AS DATE) BETWEEN ''' + CONVERT(VARCHAR(10), @startDate, 120) + ''' AND ''' + CONVERT(VARCHAR(10), @endDate, 120) + '''
     ) t
     GROUP BY t.[Date]';
-
-    FETCH NEXT FROM table_cursor INTO @currentTable, @currentTableName, @currentCommentColumn;
-
-    IF @@FETCH_STATUS = 0
-        SET @sql = @sql + '
-    UNION ALL';
-END;
 ​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
